582e14d967e862aed85ac51bfa0afcd855f6c596
Ivan Clay <ivan.clay.moura@gmail.com>
2024-01-28 09:35:06 -0300

    feat(balta io): modeling the domain

---
+++ UtmBuilder.Core/Utm.cs
@@ -0,0 +1,11 @@
+﻿using UtmBuilder.Core.ValueObjects;
+
+namespace UtmBuilder.Core
+{
+    public class Utm
+    {
+        public Url Url { get; set; }
+
+        public Campaign Campaign { get; set; }
+    }
+}
---
+++ UtmBuilder.Core/ValueObjects/Campaign.cs
@@ -0,0 +1,54 @@
+﻿namespace UtmBuilder.Core.ValueObjects
+{
+    public class Campaign : ValueObject
+    {
+        /// <summary>
+        /// Generate a new campaign for a URL
+        /// </summary>
+        /// <param name="source">The referrer (e.g. google, newsletter)</param>
+        /// <param name="medium">Marketing medium (e.g. cpc, banner, email)</param>
+        /// <param name="name">Product, promo code, or slogan (e.g. spring_sale) One of campaign name or campaign id are required.</param>
+        /// <param name="id">The ads campaign id.</param>
+        /// <param name="term">Identify the paid keywords</param>
+        /// <param name="content">Use to differentiate ads</param>
+        public Campaign(string source, string name, string medium, string? id = null, string? term = null, string? content = null) 
+        {
+            Source = source;
+            Name = name;
+            Medium = medium;
+            Id = id;
+            Term = term;
+            Content = content;
+        }
+
+        /// <summary>
+        /// The referrer (e.g. google, newsletter)
+        /// </summary>
+        public string Source { get; }
+
+        /// <summary>
+        /// Marketing medium (e.g. cpc, banner, email)
+        /// </summary>
+        public string Medium { get; }
+
+        /// <summary>
+        /// Product, promo code, or slogan (e.g. spring_sale) One of campaign name or campaign id are required.
+        /// </summary>
+        public string Name { get; }
+        
+        /// <summary>
+        /// The ads campaign id.
+        /// </summary>
+        public string? Id { get; }
+
+        /// <summary>
+        /// Identify the paid keywords
+        /// </summary>
+        public string? Term { get; }
+
+        /// <summary>
+        /// Use to differentiate ads
+        /// </summary>
+        public string? Content { get; }
+    }
+}
