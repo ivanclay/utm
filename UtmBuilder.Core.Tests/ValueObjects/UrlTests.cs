465d403f4f8ac1f05d3f6ce3578380618533bf94
Ivan Clay <ivan.clay.moura@gmail.com>
2024-01-29 08:49:48 -0300

    feat(balta io): unit tests

---
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -0,0 +1,39 @@
+﻿using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exception;
+
+namespace UtmBuilder.Core.Tests.ValueObjects;
+
+[TestClass]
+public class UrlTests
+{
+    [TestMethod(displayName: "Deve retornar exceção quando URL for inválida.")]
+    //[TestCategory("Testes de URL")]
+    public void ShouldReturnExceptionWhenUrlIsInvalid()
+    {
+        try
+        {
+            var url = new Url("");
+            Assert.Fail();
+        }
+        catch (InvalidUrlException e)
+        {
+            Assert.IsTrue(true);
+        }
+    }
+
+    [TestMethod(displayName: "Deve retornar exceção quando URL for inválida.")]
+    [ExpectedException(typeof(InvalidUrlException))]
+    //[TestCategory("Testes de URL")]
+    public void ExpectedExceptionWhenUrlIsInvalid()
+    {
+        new Url("qualquer-coisa-que-nao-seja-url-valida");
+    }
+
+    [TestMethod(displayName: "Não deve retornar exceção quando URL for válida.")]
+    //[TestCategory("Testes de URL")]
+    public void ShouldNotReturnExceptionWhenUrlIsValid()
+    {
+        var url = new Url("https://balta.io");
+        Assert.IsTrue(true);
+    }
+}
--- UtmBuilder.Core/ValueObjects/Exception/InvalidUrlException.cs
+++ UtmBuilder.Core/ValueObjects/Exception/InvalidUrlException.cs
@@ -13,12 +13,13 @@ public partial class InvalidUrlException : System.Exception
 
     public static void ThrowIfInvalid(string address, string message = DefaultErrorMessage) 
     {
-        if (string.IsNullOrEmpty(address)) {  throw new ArgumentNullException(message); }
+        if (string.IsNullOrEmpty(address)) {  throw new InvalidUrlException(message); }
 
         if (!UrlRegex().IsMatch(address)) { throw new InvalidUrlException(); }
     }
 
     //.NET 7 new resource
-    [GeneratedRegex("^(http | https):(\\\\/\\\\/www\\\\.|\\\\/\\\\/www\\\\.|\\\\/\\\\/|\\\\/\\\\/)[a-z0-9]+([\\\\-\\\\.]{1}[a-z0-9]+)*\\\\.[a-z]{2,5}(: [0 - 9]{ 1,5})? (\\\\/.*)?$|(http|https):(\\\\/\\\\/ localhost:\\\\d *|\\\\/\\\\/ 127\\\\.([0 - 9] | [1 - 9][0 - 9] | 1[0 - 9][0 - 9] | 2[0 - 4][0 - 9] | 25[0 - 5])\\\\.([0 - 9] | [1 - 9][0 - 9] | 1[0 - 9][0 - 9] | 2[0 - 4][0 - 9] | 25[0 - 5])\\\\.([0 - 9] | [1 - 9][0 - 9] | 1[0 - 9][0 - 9] | 2[0 - 4][0 - 9] | 25[0 - 5]))(: [0 - 9]{ 1,5})? (\\\\/.*)?$")]
+    //[GeneratedRegex("^(http | https):(\\\\/\\\\/www\\\\.|\\\\/\\\\/www\\\\.|\\\\/\\\\/|\\\\/\\\\/)[a-z0-9]+([\\\\-\\\\.]{1}[a-z0-9]+)*\\\\.[a-z]{2,5}(: [0 - 9]{ 1,5})? (\\\\/.*)?$|(http|https):(\\\\/\\\\/ localhost:\\\\d *|\\\\/\\\\/ 127\\\\.([0 - 9] | [1 - 9][0 - 9] | 1[0 - 9][0 - 9] | 2[0 - 4][0 - 9] | 25[0 - 5])\\\\.([0 - 9] | [1 - 9][0 - 9] | 1[0 - 9][0 - 9] | 2[0 - 4][0 - 9] | 25[0 - 5])\\\\.([0 - 9] | [1 - 9][0 - 9] | 1[0 - 9][0 - 9] | 2[0 - 4][0 - 9] | 25[0 - 5]))(: [0 - 9]{ 1,5})? (\\\\/.*)?$")]
+    [GeneratedRegex("^(http|https):(\\/\\/www\\.|\\/\\/www\\.|\\/\\/|\\/\\/)[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$|(http|https):(\\/\\/localhost:\\d*|\\/\\/127\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))(:[0-9]{1,5})?(\\/.*)?$")]
     private static partial Regex UrlRegex();
 }
